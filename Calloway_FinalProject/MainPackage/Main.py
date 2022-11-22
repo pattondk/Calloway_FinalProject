6eccea73e3bf92582424542632038c42f66ea093
dkpat <dkpat@LAPTOP-H2TQ9942.wifi.uc.edu>
2022-11-22 08:53:32 -0500

    First Commit: Main and Decrytion Packages Created

---
+++ Calloway_FinalProject/MainPackage/Main.py
@@ -0,0 +1,10 @@
+'''
+Name: David Patton, Josh Earley, Matt Lamb, Aria Nielsen
+email: pattondk@mail.uc.edu, earleyja@mail.uc.edu, 
+Assignment: Calloway_FinalProject
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: Decrypted a file and found our location presented a picture
+Citations: 
+Anything else that's relevant: This was a group project and it was fun! :)
+'''
