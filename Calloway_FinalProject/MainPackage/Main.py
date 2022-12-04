73fb5f8bc6bbd285fa8c19f98b56b024d39ff7e5
ariar <ariar@DESKTOP-PMIQ0CQ.wifi.uc.edu>
2022-12-04 18:01:21 -0500

    Added Indexing Function to Main

--- Calloway_FinalProject/MainPackage/Main.py
+++ Calloway_FinalProject/MainPackage/Main.py
@@ -8,3 +8,32 @@ Brief Description: Decrypted a file and found our location presented a picture
 Citations: 
 Anything else that's relevant: This was a group project and it was fun! :)
 '''
+#This reads the text file
+with open('english.txt') as f:
+    lines = f.readlines()
+
+#This function turns the text file into a dictionary that is indexed    
+def englishIndex(fileName):
+    d = {}
+    with open(fileName, 'r') as f:       
+        content = f.readlines()
+        lnc = 0
+        result = {}
+        for line in content:
+            line = line.rstrip()
+            words = line.split(" ")
+            for word in words:
+                tmp = result.get(word)
+                if tmp is None:
+                    result[word] = []
+                if lnc not in result[word]:
+                    result[word].append(lnc)
+
+            lnc = lnc + 1
+
+        return result
+
+print(englishIndex('english.txt'))
+
+englishIndex('english.txt')
+
