85979e653862adda9b09e9cba288b8b2f01eb586
ariar <ariar@DESKTOP-PMIQ0CQ.wifi.uc.edu>
2022-12-07 19:06:47 -0500

    Final Changes

--- Calloway_FinalProject/DecryptionPackage/Decryption.py
+++ Calloway_FinalProject/DecryptionPackage/Decryption.py
@@ -1,6 +1,6 @@
 '''
 Name: David Patton and Josh Earley
-email: pattondk@mail.uc.edu, earleyja@mail.uc.edu, 
+email: pattondk@mail.uc.edu, earleyja@mail.uc.edu, lambmj@mail.uc.edu, nielseai@mail.uc.edu, ehrlicmh@mail.uc.edu
 Assignment: Calloway_FinalProject
 Course: IS 4010
 Semester/Year: Fall 2022
@@ -8,10 +8,14 @@ Brief Description: Decrypted a file and found our location presented a picture
 Citations: 
 Anything else that's relevant: This was a group project and it was fun! :)
 '''
+from PIL import Image # This will allow us to use pillow 
+import json
+
 #This reads the text file
 with open('english.txt') as f:
     lines = f.readlines()
-
+with open('EncryptedGroupHints.json') as g:
+    lines = g.readlines()
 #This function turns the text file into a dictionary that is indexed    
 def englishIndex(fileName):
     d = {}
@@ -33,6 +37,56 @@ def englishIndex(fileName):
 
         return result
 
-print(englishIndex('english.txt'))
+#print(englishIndex('english.txt'))
 
-englishIndex('english.txt')
+#englishIndex('english.txt')
+
+def show_picture(filename):
+    
+    Neilpicture = Image.open(filename)
+    angle = 90
+    out = Neilpicture.rotate(angle)
+    out.load()
+     
+    
+    return out
+
+
+def jsonIndex(filename: str):
+    with open(filename) as f_in:
+        return json.load(f_in)
+
+if __name__ == "__main__":
+    my_data = jsonIndex('EncryptedGroupHints.json')
+    print(my_data)
+
+'''jsonIndex('EncryptedGroupHints.json')
+
+myJsonIndex = (jsonIndex('EncryptedGroupHints.json'))
+myEnglishIndex = (englishIndex('english.txt'))
+
+myEnglishIndex = dict(myEnglishIndex)
+
+
+list(myJsonIndex)
+callowayNum = (myJsonIndex['Calloway'])
+
+#print(callowayNum)
+callowayNum = [eval(i) for i in callowayNum]
+#print(callowayNum)'''
+
+'''result_dict = {}
+for ele in callowayNum:
+    result_dict[ele] = []
+    for key in myEnglishIndex.keys():
+        if ele in myEnglishIndex[key]:
+            result_dict[ele].append(key)
+
+print(result_dict)'''
+
+
+
+
+
+
+
