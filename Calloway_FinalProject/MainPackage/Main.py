2a4a28bd3b0d3bbbf07cd263f4d8f02d160e68e8
pattondk <116082224+pattondk@users.noreply.github.com>
2022-12-07 19:17:26 -0500

    Update Main.py

--- Calloway_FinalProject/MainPackage/Main.py
+++ Calloway_FinalProject/MainPackage/Main.py
@@ -1,23 +1,41 @@
 '''
-Name: David Patton, Josh Earley, Matt Lamb, Aria Nielsen, Maxwell Ehrlich 
+Name: David Patton, Josh Earley, Matt Lamb, Aria Nielsen
 email: pattondk@mail.uc.edu, earleyja@mail.uc.edu, lambmj@mail.uc.edu, nielseai@mail.uc.edu, ehrlicmh@mail.uc.edu
 Assignment: Calloway_FinalProject
 Course: IS 4010
 Semester/Year: Fall 2022
 Brief Description: Decrypted a file and found our location presented a picture
-Citations: 
+Citations: https://stackoverflow.com/questions/66171273/return-keys-of-dictionary-if-values-match-with-a-given-list
+https://www.geeksforgeeks.org/python-converting-all-strings-in-list-to-integers/
+https://stackoverflow.com/questions/41476636/how-to-read-a-json-file-and-return-as-dictionary-in-python
 Anything else that's relevant: This was a group project and it was fun! :)
 '''
-
 from DecryptionPackage.Decryption import *
 from PIL import Image
 
-print(englishIndex)
+
+jsonIndex('EncryptedGroupHints.json')
+
+myJsonIndex = (jsonIndex('EncryptedGroupHints.json'))
+myEnglishIndex = (englishIndex('english.txt'))
+
+myEnglishIndex = dict(myEnglishIndex)
+
+
+list(myJsonIndex)
+callowayNum = (myJsonIndex['Calloway'])
+
+callowayNum = [eval(i) for i in callowayNum]
+
+decodedLoc = {}
+for ele in callowayNum:
+    decodedLoc[ele] = []
+    for key in myEnglishIndex.keys():
+        if ele in myEnglishIndex[key]:
+            decodedLoc[ele].append(key)
+
+
+print(decodedLoc.values())
 
 Neil = Image.open("Neil.jpg")
 Neil.show()
-
-from DecryptionPackage.Decryption import *
-
-print(englishIndex)
-
