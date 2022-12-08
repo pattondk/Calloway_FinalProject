72858d91f2f81c2be1926c58d695d5fd241476c9
pattondk <116082224+pattondk@users.noreply.github.com>
2022-12-07 19:20:48 -0500

    Update Decryption.py

--- Calloway_FinalProject/DecryptionPackage/Decryption.py
+++ Calloway_FinalProject/DecryptionPackage/Decryption.py
@@ -1,11 +1,13 @@
 '''
-Name: David Patton and Josh Earley
+Name: David Patton, Josh Earley, Matt Lamb, Aria Nielsen, Maxwell Ehrlich 
 email: pattondk@mail.uc.edu, earleyja@mail.uc.edu, lambmj@mail.uc.edu, nielseai@mail.uc.edu, ehrlicmh@mail.uc.edu
 Assignment: Calloway_FinalProject
 Course: IS 4010
 Semester/Year: Fall 2022
 Brief Description: Decrypted a file and found our location presented a picture
-Citations: 
+Citations: https://stackoverflow.com/questions/66171273/return-keys-of-dictionary-if-values-match-with-a-given-list
+https://www.geeksforgeeks.org/python-converting-all-strings-in-list-to-integers/
+https://stackoverflow.com/questions/41476636/how-to-read-a-json-file-and-return-as-dictionary-in-python
 Anything else that's relevant: This was a group project and it was fun! :)
 '''
 from PIL import Image # This will allow us to use pillow 
