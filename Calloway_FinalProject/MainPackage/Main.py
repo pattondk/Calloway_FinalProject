0abd472130a572a0c2509a8bbbc8bec92824750e
pattondk <116082224+pattondk@users.noreply.github.com>
2022-12-07 19:19:32 -0500

    Update Main.py

--- Calloway_FinalProject/MainPackage/Main.py
+++ Calloway_FinalProject/MainPackage/Main.py
@@ -1,5 +1,5 @@
 '''
-Name: David Patton, Josh Earley, Matt Lamb, Aria Nielsen
+Name: David Patton, Josh Earley, Matt Lamb, Aria Nielsen, Maxwell Ehrlich 
 email: pattondk@mail.uc.edu, earleyja@mail.uc.edu, lambmj@mail.uc.edu, nielseai@mail.uc.edu, ehrlicmh@mail.uc.edu
 Assignment: Calloway_FinalProject
 Course: IS 4010
